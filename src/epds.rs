230fb947cbb62e325211c0124750aa691633be70
Christoph Groß <christoph.gross@student.uni-tuebingen.de>
2018-08-06 10:03:35 +0200

    Added a general display description struct (epds::epd)

--- src/epds.rs
+++ src/epds.rs
@@ -1,2 +1,21 @@
 
-//TODO: fill
+/// A struct containing necessary info about a epd (electronic paper display). E.g:
+/// 
+/// - Width
+/// - Height
+/// ...
+/// 
+/// This needs to be implemented by each new Display
+pub struct EPD {
+    pub(crate) width: u16,
+    pub(crate) height: u16
+    //displayrotation?
+}
+
+impl EPD {
+    pub(crate) fn new(width: u16, height: u16) -> EPD {
+        EPD {width, height}
+    }
+
+
+}
