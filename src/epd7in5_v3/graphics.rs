8a5aa031c85e63ef01d4c1d5e531d2d26f775b02
Andrew Thomas <andrewjamest1993@gmail.com>
2022-05-21 01:28:50 -0500

    Remove BinaryColor-only tests for 7in5 V3

--- src/epd7in5_v3/graphics.rs
+++ src/epd7in5_v3/graphics.rs
@@ -99,7 +99,7 @@ mod tests {
     use super::*;
     use crate::color::{Black, Color};
     use crate::epd7in5_v3;
-    use crate::graphics::{Display, DisplayRotation};
+    use crate::graphics::{DisplayRotation};
     use embedded_graphics::{
         prelude::*,
         primitives::{Line, PrimitiveStyle},
@@ -109,7 +109,7 @@ mod tests {
     #[test]
     fn graphics_size() {
         let display = Display7in5::default();
-        assert_eq!(display.buffer().len(), 48000);
+        assert_eq!(display.buffer().len(), 96000);
     }
 
     // test default background color on all bytes
@@ -117,77 +117,6 @@ mod tests {
     fn graphics_default() {
         let display = Display7in5::default();
         for &byte in display.buffer() {
-            assert_eq!(byte, epd7in5_v3::DEFAULT_BACKGROUND_COLOR.get_byte_value());
-        }
-    }
-
-    #[test]
-    fn graphics_rotation_0() {
-        let mut display = Display7in5::default();
-
-        let _ = Line::new(Point::new(0, 0), Point::new(7, 0))
-            .into_styled(PrimitiveStyle::with_stroke(Black, 1))
-            .draw(&mut display);
-
-        let buffer = display.buffer();
-
-        assert_eq!(buffer[0], Color::Black.get_byte_value());
-
-        for &byte in buffer.iter().skip(1) {
-            assert_eq!(byte, epd7in5_v3::DEFAULT_BACKGROUND_COLOR.get_byte_value());
-        }
-    }
-
-    #[test]
-    fn graphics_rotation_90() {
-        let mut display = Display7in5::default();
-        display.set_rotation(DisplayRotation::Rotate90);
-
-        let _ = Line::new(Point::new(0, 792), Point::new(0, 799))
-            .into_styled(PrimitiveStyle::with_stroke(Black, 1))
-            .draw(&mut display);
-
-        let buffer = display.buffer();
-
-        assert_eq!(buffer[0], Color::Black.get_byte_value());
-
-        for &byte in buffer.iter().skip(1) {
-            assert_eq!(byte, epd7in5_v3::DEFAULT_BACKGROUND_COLOR.get_byte_value());
-        }
-    }
-
-    #[test]
-    fn graphics_rotation_180() {
-        let mut display = Display7in5::default();
-        display.set_rotation(DisplayRotation::Rotate180);
-
-        let _ = Line::new(Point::new(792, 479), Point::new(799, 479))
-            .into_styled(PrimitiveStyle::with_stroke(Black, 1))
-            .draw(&mut display);
-
-        let buffer = display.buffer();
-
-        assert_eq!(buffer[0], Color::Black.get_byte_value());
-
-        for &byte in buffer.iter().skip(1) {
-            assert_eq!(byte, epd7in5_v3::DEFAULT_BACKGROUND_COLOR.get_byte_value());
-        }
-    }
-
-    #[test]
-    fn graphics_rotation_270() {
-        let mut display = Display7in5::default();
-        display.set_rotation(DisplayRotation::Rotate270);
-
-        let _ = Line::new(Point::new(479, 0), Point::new(479, 7))
-            .into_styled(PrimitiveStyle::with_stroke(Black, 1))
-            .draw(&mut display);
-
-        let buffer = display.buffer();
-
-        assert_eq!(buffer[0], Color::Black.get_byte_value());
-
-        for &byte in buffer.iter().skip(1) {
             assert_eq!(byte, epd7in5_v3::DEFAULT_BACKGROUND_COLOR.get_byte_value());
         }
     }
