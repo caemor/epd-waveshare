eb625aabffc8bf173dd2b59cfa8c9851e4145af4
Christoph Gross <11088935+caemor@users.noreply.github.com>
2024-11-04 11:15:23 +0100

    Update src/traits.rs

--- src/traits.rs
+++ src/traits.rs
@@ -136,7 +136,7 @@ where
 ///
 ///```rust, no_run
 ///# use epd_waveshare::epd4in2::Display4in2;
-///# use embedded_graphics_core::prelude::*;
+///# use embedded_graphics::prelude::*;
 ///let mut display = Box::new(Display4in2::default());
 ///let _ = Line::new(Point::new(0, 120), Point::new(0, 295))
 ///     .into_styled(PrimitiveStyle::with_stroke(Color::Black, 1))
